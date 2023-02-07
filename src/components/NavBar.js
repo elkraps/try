f91a3af4f2d13915e8d2dbf4fd6292bf7485d63d
Евгения <ilinaevgeniya@mail.ru>
2023-02-08 00:47:55 +0300

    5

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React from 'react';
 // import {Context} from "../index";
 import Navbar from "react-bootstrap/Navbar";
 // import Nav from "react-bootstrap/Nav";
